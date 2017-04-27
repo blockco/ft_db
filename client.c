4a2b6912f7123b7074f61faeff83a202eab9e5a0
robert passafaro <rpassafa@e1z2r3p14.42.us.org>
2017-04-26 19:02:21 -0700

    added server and client

---
+++ client.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/17 19:08:47 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/17 19:08:50 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	int					sockfd;
+	char				recvline[100];
+	char 				str[100];
+	struct sockaddr_in	servaddr;
+
+	bzero(str, 100);
+	bzero(recvline, 100);
+	printf("%s\n", "write echo phrase");
+	scanf("%s", str);
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(22000);
+	inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
+	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	write(sockfd, str, strlen(str));
+	read(sockfd, recvline, 100);
+	write(1, recvline, strlen(recvline));
+}
---
+++ server.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/17 19:11:10 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/17 19:12:48 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int	main(void)
+{
+	char				str[100];
+	int					listen_fd;
+	int					comm_fd;
+	struct sockaddr_in	servaddr;
+
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+	servaddr.sin_port = htons(22000);
+	bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	listen(listen_fd, 10);
+	while (1)
+	{
+		bzero(str, 100);
+		comm_fd = accept(listen_fd, (struct sockaddr*)NULL, NULL);
+		read(comm_fd, str, 100);
+		printf("%s\n", str);
+		write(comm_fd, str, strlen(str));
+	}
+}
